0077496172dce3e4eb083855f8599c976b236564
GeekyPRAVEE <l.k.pravee@gmail.com>
2020-12-11 17:54:52 +0530

    whatssappchatadded

---
+++ index.js
@@ -0,0 +1,15 @@
+<script type="text/javascript">
+   $(function () {
+           $('#WAButton').floatingWhatsApp({
+               phone: '919080564664', //WhatsApp Business phone number
+               headerTitle: 'Chat with us on WhatsApp!', //Popup Title
+               popupMessage: 'Hello, how can we help you?', //Popup Message
+               showPopup: true, //Enables popup display
+               buttonImage: '<img src="whatsapp.svg" />', //Button Image
+               //headerColor: 'crimson', //Custom header color
+               //backgroundColor: 'crimson', //Custom background button color
+               position: "right" //Position: left | right
+
+           });
+       });
+</script>
